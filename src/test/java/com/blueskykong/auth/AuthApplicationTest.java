98666f2cb31e40cdab536dbdd155d4ff51e9db67
keets2012 <928420597@qq.com>
2017-12-11 21:41:47 +0800

    update readme

---
+++ src/test/java/com/blueskykong/auth/AuthApplicationTest.java
@@ -0,0 +1,18 @@
+package com.blueskykong.auth;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author keets
+ * @date 2016/12/5
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthApplicationTest {
+
+
+}

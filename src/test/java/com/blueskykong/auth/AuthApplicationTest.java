840f8ea0ec30426c3c6f588d20ae8b555b01c362
keets2012 <928420597@qq.com>
2018-06-26 15:23:44 +0800

    add Travis CI and update unit test

--- src/test/java/com/blueskykong/auth/AuthApplicationTest.java
+++ src/test/java/com/blueskykong/auth/AuthApplicationTest.java
@@ -1,9 +1,22 @@
 package com.blueskykong.auth;
 
 
+import com.blueskykong.auth.dao.ClientSecretDAO;
+import com.blueskykong.auth.dto.ApiClientDTO;
+import com.blueskykong.auth.entity.ClientSecret;
+import com.blueskykong.auth.service.ClientSecretService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author keets
@@ -13,6 +26,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthApplicationTest {
+
+    @Mock
+    private ClientSecretDAO clientSecretDao;
+
+    @Mock
+    private ClientSecretService clientSecretService;
+
+    @Before
+    public void setUp() {
+        ApiClientDTO mockClient = new ApiClientDTO();
+        mockClient.setClientId("test");
+        mockClient.setClientSecret("test");
+        mockClient.setPurpose("test..");
+        ClientSecret clientSecret = new ClientSecret.ClientSecretBuilder()
+                .withTenantId(UUID.randomUUID())
+                .build();
+        when(clientSecretService.getClientSecretByClientId("test")).thenReturn(mockClient);
+
+//        when(clientSecretDao.get(clientSecret)).thenReturn()
+    }
+
+    @Test
+    public void getClientSecretByClientIdTest() {
+        ApiClientDTO apiClientDTO = clientSecretService.getClientSecretByClientId("test");
+        Assert.assertEquals(apiClientDTO.getClientSecret(),"test" );
+    }
 
 
 }
